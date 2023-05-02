297add820f78f93de43264ec68fa1063ffce1c2d
Joao Guilherme <joaoguilhermefagundes@gmail.com>
2023-05-02 16:48:29 -0300

    continuacao

--- src/fixpay/fixpay.service.ts
+++ src/fixpay/fixpay.service.ts
@@ -1,42 +1,22 @@
 import { HttpService } from '@nestjs/axios';
 import { catchError, last, lastValueFrom } from 'rxjs';
 import { Body, HttpException, Injectable } from '@nestjs/common';
+import { Data } from './modelo/fixpay';
 
 @Injectable()
 export class FixpayService {
   constructor(private readonly httpService: HttpService) { }
 
-  async geraPix(@Body() body: {}): Promise<any> {
+  async geraPix(body: {}) {
+    console.log(body);
     const pix = await lastValueFrom(
-      this.httpService.post('https://pix.fixpay.com.br:3467/v1/generate_pix_per_day', body, {
-        headers: {
-          Accept: 'application/json',
-          Authorization: `Bearer ${process.env.TOKEN}`
-        }
-      }).pipe(
-        catchError((e) => {
-          console.log('erro', e.response?.data);
-          throw new HttpException(
-            e.response?.data?.error?.message,
-            e.response?.status || 500,
-          );
-        })
-      ))
-    const { data } = pix.data
-    return this.qrCode(data);
-  }
-
-  async qrCode(pix) {
-    if(!pix) {
-      return this.geraPix(pix)
-    }
-    const geraQRCode = await lastValueFrom(
-      this.httpService.get(`https://pix.fixpay.com.br:3467/v1/consult_pix_qrcode/${pix.txid}`, {
-        headers: {
-          Accept: 'application/json',
-          Authorization: `Bearer ${process.env.TOKEN}`
-        }
-      }).pipe(
+      this.httpService.post<Data>('https://pix.fixpay.com.br:3467/v1/generate_pix_per_day', body,
+        {
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${process.env.TOKEN}`
+          }
+        }).pipe(
           catchError((e) => {
             console.log('erro', e.response?.data);
             throw new HttpException(
@@ -44,8 +24,32 @@ export class FixpayService {
               e.response?.status || 500,
             );
           })
-        )
-    )
-    console.log(geraQRCode)
-  }
+        ));
+
+    if (pix) {
+      for (const txId of pix.data) {
+        if (!txId.txid) {
+          return 'Nenhuma chave gerada'
+        } 
+        console.log(txId.txid);
+        await this.qrCode(txId.txid)
+    }
+  };
 }
+  async qrCode(pix) {
+      console.log(pix)
+      if (!this.qrCode(pix)) {
+        return this.geraPix
+      }
+      const qrCode = await lastValueFrom(
+        this.httpService.get(`https://pix.fixpay.com.br:3467/v1/consult_pix_qrcode/${pix.txid}`,
+          {
+            headers: {
+              Accept: 'application/json',
+              Authorization: `Bearer ${process.env.TOKEN}`
+            }
+          })
+      )
+      console.log(qrCode)
+    }
+  }
